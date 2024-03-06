7531de399af1e570e16fe0163d23241679a39280
angelo morales <angelotuba3@gmail.com>
2024-03-06 16:57:47 +0100

    updte test

--- make_test.js
+++ make_test.js
@@ -3,4 +3,28 @@ export function evenOrOdd(p = 0) {
   return p % 2 ? 'odd [Impar]' : 'even [Par]';
 }
 
-export function factorial() {}
+export function factorial(p) {
+  if (p < 0) {
+    throw new Error('numero negagivo');
+  }
+  if (p !== Math.trunc(p)) {
+    throw new Error('numero negagivo');
+  }
+
+  let r = 1;
+  for (let i = 2; i <= p; i++) {
+    r *= i;
+  }
+  return r;
+}
+
+// funtion like array.length
+
+export const arrayLength = (array) => {
+  let r = 0;
+  for (const iterator of array) {
+    r++;
+  }
+
+  return r;
+};
--- make_test.test.js
+++ make_test.test.js
@@ -1,14 +1,14 @@
-import { evenOrOdd } from './make_test.js';
+import { arrayLength, evenOrOdd, factorial } from './make_test.js';
 
 describe('evenOrOdd', () => {
   test('ir should return "odd [Impar]" when parametear is 3', () => {
-    //arrange;
+    //arrange valores ;
     const r = 3;
-    const expected = 'odd [Impar]';
+    const expected = 'odd [Impar]'; // lo que espera
     // act (actualizar la funcion)
-    const x = evenOrOdd(r);
+    const result = evenOrOdd(r);
     //assert
-    expect(x).toBe(expected);
+    expect(factorial(0)).toBe(expected(1)); // esta es la manera de resumirlo en una linea
   });
   test('ir should return "even [Par]" when parametear is 2', () => {
     //arrange;
@@ -32,7 +32,68 @@ describe('evenOrOdd', () => {
 });
 
 describe('factorial', () => {
-  test('should first', () => {
-    second;
+  test('it should return 1 when argument is 0', () => {
+    const x = 0;
+    const expected = 1;
+    const result = factorial(x);
+    expect(result).toBe(expected);
+  });
+
+  test('it should return 1 when argument is 1', () => {
+    const x = 1;
+    const expected = 1;
+    const result = factorial(x);
+    expect(result).toBe(expected);
+  });
+
+  test('it should return 120 when argument is 5', () => {
+    const x = 5;
+    const expected = 120;
+    const result = factorial(x);
+    expect(result).toBe(expected);
+  });
+
+  test('it should ... when argument is -5', () => {
+    const x = -5;
+    const expected = 'numero negativo';
+    const result = factorial(x);
+    expect(result).toBe(expected);
+  });
+
+  test('it should ... when argument is 5,5', () => {
+    const x = 5.5;
+    const expected = 'numero negativo';
+    expect(result).toBe(expected);
+    expect(() => factorial(x)).toThrow(); //manera de
+  });
+});
+
+describe.only('arrayLength', () => {
+  test('it should be 0 when argument is []', () => {
+    const x = [];
+    const expected = 0;
+    const r = arrayLength(x);
+    expect(r).toBe(expected);
+  });
+
+  test('it should be 1 when argument is [0]', () => {
+    const x = [6];
+    const expected = 1;
+    const r = arrayLength(x);
+    expect(r).toBe(expected);
+  });
+
+  test('it should be 3 when argument is [6,6,6]', () => {
+    const x = [6, 6, 6];
+    const expected = 3;
+    const r = arrayLength(x);
+    expect(r).toBe(expected);
+  });
+
+  test('it should be 1 when argument is [false,false,false]', () => {
+    const x = [false, false, false];
+    const expected = 3;
+    const r = arrayLength(x);
+    expect(r).toBe(expected);
   });
 });
